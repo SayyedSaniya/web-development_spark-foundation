e4c1575320262e8d3db02d9b398bbc73edf5294c
Sayyed Saniya <80396520+SayyedSaniya@users.noreply.github.com>
2021-05-04 05:44:47 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Earth help</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <header class="header">
+      <div class="background-overlay">
+        <nav class="navbar navbar-expand-lg bg-dark">
+          <a class="navbar-brand text-success" href="#"> 
+            <h1 style="color: orange">Earth Help</h1>
+          </a> 
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <a class="btn btn-primary float-right" href="payment.php" role="button">Donate Now</a>
+          </div>
+        </nav>
+        <div class="jumbotron jumbotron-fluid text-center">
+            <div class="jumbotron">
+              <h1>Help Us To</h1>
+              <h2>To Help you....!!!</h2>
+            </div>
+        </div>
+      </div>
+    </header>
+  </body>
+</html>
